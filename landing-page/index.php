233d2bed1ab0b9a90b342f36f7ff7da058437ad8
Luis Pulido <luis@luispulido.com>
2013-08-14 10:30:36 -0700

    changes

--- landing-page/index.php
+++ landing-page/index.php
@@ -4,6 +4,9 @@
 		<meta charset="utf-8"/>
 		<title>Ignacio de la Llave - The best mexican roasted coffe</title>
 		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" href="favicon.ico" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="description" content="Feshly and perfectly roasted coffee to bring to your table a clean, fruity and nutty cup of this marvelous beverage." />
 		<link rel="stylesheet" href="style.css"/>
 	</head>
 	<body>
