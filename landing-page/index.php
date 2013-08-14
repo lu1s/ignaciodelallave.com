a068908799a5afd2653311112662f977a7e05f2d
Luis Pulido <luis@luispulido.com>
2013-08-14 10:14:52 -0700

    Landing page

---
+++ landing-page/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Ignacio de la Llave - The best mexican roasted coffe</title>
+		<link href='http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="style.css"/>
+	</head>
+	<body>
+		<div class="description">
+			<h1>Ignacio de la Llave</h1>
+			<p>Ignacio will bring to you the best coffee beans from Veracruz, Mexico.
+			<p>Every day you will find feshly and perfectly roasted coffee to bring to your table a clean, fruity and nutty cup of this marvelous beverage.</p>
+		</div>
+		<div id="bird">
+
+		</div>
+	</body>
+</html>
